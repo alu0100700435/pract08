2e171ec09017243da02728acbc863ad56c9b6c22
Raquel Alvarez <alu0100700435@ull.edu.es>
2013-11-05 20:53:13 +0000

    sobrecarga de *

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -11,4 +11,25 @@ class Matriz
     @matriz[row,col]
   end
   
+  def []=(row,col, value)
+    @matriz[row,col] = value
+  end
+  
+  def *(other)
+    raise TypeError "Las matrices no pueden multiplicarse" unless (@col ==
+other.row)
+    
+    matres = Matriz.new(@row,@col)
+    for i in 0...@row
+      for j in 0...other.col
+	for k in 0...@col
+	  matres[i][j]+=@matriz[i][k]*other[k][j]
+	end
+      end
+    end
+    matres
+  end
+  
+  
+  
 end
